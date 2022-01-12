151a8453780528573e3afee6a335942219b6071b
Justin Ho <justinho@jcsh.dev>
2022-01-11 21:54:50 -0800

    refactor float into new type struct

--- src/tuples.rs
+++ src/tuples.rs
@@ -1,13 +1,14 @@
 use std::mem::discriminant;
+use std::ops::Add;
 
 /// Base type to represent the coordinate system
 /// (uses the left-handed coordinate system)
 #[derive(Debug)]
 pub struct TupleData {
-    pub x: f64,
-    pub y: f64,
-    pub z: f64,
-    pub w: f64,
+    pub x: Float,
+    pub y: Float,
+    pub z: Float,
+    pub w: Float,
 }
 
 /// Type class to represent the two variations of Tuple
@@ -24,9 +25,54 @@ impl PartialEq for Tuple {
 }
 
 pub fn point(x: f64, y: f64, z: f64) -> Tuple {
-    Tuple::Point(TupleData { x, y, z, w: 1.0 })
+    Tuple::Point(TupleData {
+        x: Float::from(x),
+        y: Float::from(y),
+        z: Float::from(z),
+        w: Float::from(1.0),
+    })
 }
 
 pub fn vector(x: f64, y: f64, z: f64) -> Tuple {
-    Tuple::Vector(TupleData { x, y, z, w: 0.0 })
+    Tuple::Vector(TupleData {
+        x: Float::from(x),
+        y: Float::from(y),
+        z: Float::from(z),
+        w: Float::from(0.0),
+    })
+}
+
+/// New-type wrapper for f64
+/// implements comparisons against f64
+#[derive(Copy, Clone, Debug)]
+pub struct Float(f64);
+
+/// Constant for floating-point number comparisons
+/// precision is enough for ray-tracer
+const EPSILON: f64 = 0.00001;
+
+impl From<f64> for Float {
+    fn from(value: f64) -> Self {
+        Float(value)
+    }
+}
+
+impl PartialEq<Self> for Float {
+    fn eq(&self, other: &Self) -> bool {
+        (self.0 - other.0).abs() < EPSILON
+    }
+}
+
+impl PartialEq<f64> for Float {
+    fn eq(&self, other: &f64) -> bool {
+        (self.0 - other).abs() < EPSILON
+    }
+}
+
+impl Add<Self> for Float {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Float::from(self.0 + rhs.0)
+    }
 }
